0608f89a5a5c053c6edf77c4d85c2febfc5b552d
Jim McAndrew <>
2020-12-15 11:25:51 -0600

    Added camera photo modes

--- DronelinkParrot/DronelinkParrot.swift
+++ DronelinkParrot/DronelinkParrot.swift
@@ -216,6 +216,9 @@ extension Kernel.CameraPhotoMode {
         case .ehdr: return .single
         case .hyperLight: return .single
         case .unknown: return .single
+        case .highResolution: return .single
+        case .smart: return .single
+        case .internalAISpotChecking: return .single
         }
     }
 }
